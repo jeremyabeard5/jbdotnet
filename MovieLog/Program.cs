c156c9227e538bd5cf7b6c15c7710f54bb2c8b43
jeremyab5 <jeremyab5@gmail.com>
2021-11-20 17:40:02 -0700

    started MovieLog project

---
+++ MovieLog/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace MovieLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Movie Log");
+            String moviedir = "/media/jbmedia/Movies";
+            Console.WriteLine(moviedir);
+            Console.WriteLine("Reading files from following directory:\n" + moviedir + "\n");
+            string[] allfiles = Directory.GetFiles(moviedir, "*.*", SearchOption.AllDirectories);
+            for (int i = 0; i < allfiles.Length; i++) {
+                Console.WriteLine(allfiles[i]);
+            }
+            Console.WriteLine("\nFinished.");
+        }
+    }
+}
